1161ee5788d4658545e7c703d76f6b3ae19e8e5a
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:17:59 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,35 +1,30 @@
-output "key_vault_certificate_issuers" {
-  description = "All key_vault_certificate_issuer resources"
-  value       = azurerm_key_vault_certificate_issuer.key_vault_certificate_issuers
-  sensitive   = true
-}
 output "key_vault_certificate_issuers_account_id" {
-  description = "List of account_id values across all key_vault_certificate_issuers"
-  value       = [for k, v in azurerm_key_vault_certificate_issuer.key_vault_certificate_issuers : v.account_id]
+  description = "Map of account_id values across all key_vault_certificate_issuers, keyed the same as var.key_vault_certificate_issuers"
+  value       = { for k, v in azurerm_key_vault_certificate_issuer.key_vault_certificate_issuers : k => v.account_id }
 }
 output "key_vault_certificate_issuers_admin" {
-  description = "List of admin values across all key_vault_certificate_issuers"
-  value       = [for k, v in azurerm_key_vault_certificate_issuer.key_vault_certificate_issuers : v.admin]
+  description = "Map of admin values across all key_vault_certificate_issuers, keyed the same as var.key_vault_certificate_issuers"
+  value       = { for k, v in azurerm_key_vault_certificate_issuer.key_vault_certificate_issuers : k => v.admin }
 }
 output "key_vault_certificate_issuers_key_vault_id" {
-  description = "List of key_vault_id values across all key_vault_certificate_issuers"
-  value       = [for k, v in azurerm_key_vault_certificate_issuer.key_vault_certificate_issuers : v.key_vault_id]
+  description = "Map of key_vault_id values across all key_vault_certificate_issuers, keyed the same as var.key_vault_certificate_issuers"
+  value       = { for k, v in azurerm_key_vault_certificate_issuer.key_vault_certificate_issuers : k => v.key_vault_id }
 }
 output "key_vault_certificate_issuers_name" {
-  description = "List of name values across all key_vault_certificate_issuers"
-  value       = [for k, v in azurerm_key_vault_certificate_issuer.key_vault_certificate_issuers : v.name]
+  description = "Map of name values across all key_vault_certificate_issuers, keyed the same as var.key_vault_certificate_issuers"
+  value       = { for k, v in azurerm_key_vault_certificate_issuer.key_vault_certificate_issuers : k => v.name }
 }
 output "key_vault_certificate_issuers_org_id" {
-  description = "List of org_id values across all key_vault_certificate_issuers"
-  value       = [for k, v in azurerm_key_vault_certificate_issuer.key_vault_certificate_issuers : v.org_id]
+  description = "Map of org_id values across all key_vault_certificate_issuers, keyed the same as var.key_vault_certificate_issuers"
+  value       = { for k, v in azurerm_key_vault_certificate_issuer.key_vault_certificate_issuers : k => v.org_id }
 }
 output "key_vault_certificate_issuers_password" {
-  description = "List of password values across all key_vault_certificate_issuers"
-  value       = [for k, v in azurerm_key_vault_certificate_issuer.key_vault_certificate_issuers : v.password]
+  description = "Map of password values across all key_vault_certificate_issuers, keyed the same as var.key_vault_certificate_issuers"
+  value       = { for k, v in azurerm_key_vault_certificate_issuer.key_vault_certificate_issuers : k => v.password }
   sensitive   = true
 }
 output "key_vault_certificate_issuers_provider_name" {
-  description = "List of provider_name values across all key_vault_certificate_issuers"
-  value       = [for k, v in azurerm_key_vault_certificate_issuer.key_vault_certificate_issuers : v.provider_name]
+  description = "Map of provider_name values across all key_vault_certificate_issuers, keyed the same as var.key_vault_certificate_issuers"
+  value       = { for k, v in azurerm_key_vault_certificate_issuer.key_vault_certificate_issuers : k => v.provider_name }
 }
 
